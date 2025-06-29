4e08e57482a71468a4a79b9c0615d8b5ecce20cb
MedusaVM <ossiel.villanueva.m@gmail.com>
2025-06-28 19:01:52 -0600

    Initial version of our Node server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-import express from 'express';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//* Middleware *//
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGIN, //Only the origin defined in the env file is allowed
+  })
+);
+app.use(express.json());
+console.log(process.env);
+
+app.get("/", (_req, res) => {
+  res.send("Banana Bread Tracker backend is running!");
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
